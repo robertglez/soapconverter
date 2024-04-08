77328e4863689235118728bdbfb58727e9f58092
Roberto G <robertglez@users.noreply.github.com>
2024-04-09 00:30:34 +0200

    Update on node-fetch import

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,21 +1,23 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
-  try {
-    const response = await fetch('https://www.dataaccess.com/webservicesserver/NumberConversion.wso', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
-        // Additional headers as required by the SOAP API
-      },
-      body: event.body // Pass the SOAP request body received from the client
-    });
-    const data = await response.text();
-    return {
-      statusCode: 200,
-      body: data
-    };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
+    let fetch;
+    try {
+        if (!fetch) {
+            fetch = (await import('node-fetch')).default;
+        }
+
+        const response = await fetch('https://www.dataaccess.com/webservicesserver/NumberConversion.wso', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/xml; charset=utf-8',
+            },
+            body: event.body // Pass the SOAP request body received from the client
+        });
+        const data = await response.text();
+        return {
+            statusCode: 200,
+            body: data
+        };
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() };
+    }
 };
