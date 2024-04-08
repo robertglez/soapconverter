c2dda47f00abd0e8c5fa8a9a8a80f93aaa30d89e
Roberto G <robertglez@users.noreply.github.com>
2024-04-09 00:18:58 +0200

    Update to catch error

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,17 +1,21 @@
-// Netlify Functions: /netlify/functions/proxy.js
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-  const response = await fetch('https://www.dataaccess.com/webservicesserver/NumberConversion.wso', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'text/xml; charset=utf-8',
-    },
-    body: event.body // Forward the SOAP request from the client
-  });
-  const data = await response.text();
-  return {
-    statusCode: 200,
-    body: data
-  };
+  try {
+    const response = await fetch('https://www.dataaccess.com/webservicesserver/NumberConversion.wso', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/xml; charset=utf-8',
+        // Additional headers as required by the SOAP API
+      },
+      body: event.body // Pass the SOAP request body received from the client
+    });
+    const data = await response.text();
+    return {
+      statusCode: 200,
+      body: data
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
 };
